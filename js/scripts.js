9655018c074240e70e73c108f48b1744f627b0d8
Courtney Phillips <courtney@epicodus.com>
2018-10-15 13:54:10 -0700

    Create AddressBook constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// Business Logic for AddressBook ---------
+
+function AddressBook() {
+  this.contacts = []
+}
+
+// Business Logic for Contacts ---------
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
   this.lastName = lastName,
