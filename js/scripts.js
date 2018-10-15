b184e15b868f086130d475419bbf6a7fda54c57c
Courtney Phillips <courtney@epicodus.com>
2018-10-15 14:55:28 -0700

    Add findContact() method to locate Contact entry in AddressBook based on unique ID.

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,15 @@ AddressBook.prototype.assignId = function() {
   return this.currentId
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    };
+  return false;
+}
+
 // Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
