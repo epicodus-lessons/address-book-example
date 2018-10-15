621d489c39275fedc430d279cc888464e47adbdf
Courtney Phillips <courtney@epicodus.com>
2018-10-15 14:35:57 -0700

    Add assignId() prototype for AddressBook to assign each inserted Contact a unique ID.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 // Business Logic for AddressBook ---------
 function AddressBook() {
-  this.contacts = [],
+  this.contacts = []
   this.currentId = 0
 }
 
 AddressBook.prototype.addContact = function(contact) {
   this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1
+  return this.currentId
 }
 
 // Business Logic for Contacts ---------
